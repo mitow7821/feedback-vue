d004e378f53853398e0ef6a2b4ae58dc1b8aa8ff
Dawid Stefanko <dawid.stefanko@grupa.itdesk.eu>
2022-09-30 12:22:06 +0200

    Changed files name

--- src/views/feedback/feedback.routes.ts
+++ src/views/feedback/feedback.routes.ts
@@ -1,11 +1,11 @@
 import { RouteRecordRaw } from "vue-router";
 
-const AddFeedbackForm = () => import("./modules/add/AddFeedbackForm.vue");
-const EditFeedbackForm = () => import("./modules/edit/EditFeedbackForm.vue");
+const AddFeedbackView = () => import("./modules/add/AddFeedbackView.vue");
+const EditFeedbackView = () => import("./modules/edit/EditFeedbackView.vue");
 const PreviewView = () => import("./modules/preview/PreviewView.vue");
 
 export default [
   { path: ":id", component: PreviewView },
-  { path: "add", component: AddFeedbackForm },
-  { path: "edit/:id", component: EditFeedbackForm },
+  { path: "add", component: AddFeedbackView },
+  { path: "edit/:id", component: EditFeedbackView },
 ] as RouteRecordRaw[];
